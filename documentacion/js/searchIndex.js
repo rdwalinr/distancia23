f13d4478cb7cc55ee56c51a0965af35f21269ff4
rdwalinr <40044704+rdwalinr@users.noreply.github.com>
2023-05-27 12:10:00 +0200

    Archivos Actividad 2
    Se nos pide subir los directorios "scripts" y "documentación" creados en el ejercicio 2 de la tarea.

---
+++ documentacion/js/searchIndex.js
@@ -0,0 +1,39 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\Calculadora",
+            "name": "Calculadora",
+            "summary": "Clase\u0020Calculadora",
+            "url": "classes/Calculadora.html"
+        },                {
+            "fqsen": "\\Calculadora\u003A\u003Amultiplicacion\u0028\u0029",
+            "name": "multiplicacion",
+            "summary": "M\u00E9todo\u0020multiplicacion",
+            "url": "classes/Calculadora.html#method_multiplicacion"
+        },                {
+            "fqsen": "\\Calculadora\u003A\u003Adivision\u0028\u0029",
+            "name": "division",
+            "summary": "M\u00E9todo\u0020division",
+            "url": "classes/Calculadora.html#method_division"
+        },                {
+            "fqsen": "\\suma\u0028\u0029",
+            "name": "suma",
+            "summary": "Esta\u0020funci\u00F3n\u0020devuelve\u0020la\u0020suma\u0020de\u0020los\u0020argumentos\nenviados\u0020en\u0020la\u0020llamada",
+            "url": "namespaces/default.html#function_suma"
+        },                {
+            "fqsen": "\\resta\u0028\u0029",
+            "name": "resta",
+            "summary": "Esta\u0020funci\u00F3n\u0020devuelve\u0020la\u0020resta\u0020de\u0020los\u0020argumentos\nenviados\u0020en\u0020la\u0020llamada",
+            "url": "namespaces/default.html#function_resta"
+        },                {
+            "fqsen": "\\saluda\u0028\u0029",
+            "name": "saluda",
+            "summary": "Esta\u0020funci\u00F3n\u0020devuelve\u0020un\u0020saludo\u0020con\u0020el\u0020nombre\u0020que\u0020le\nenviamos\u0020en\u0020la\u0020llamada",
+            "url": "namespaces/default.html#function_saluda"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
---
+++ scripts/script2-RUIZ.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * script2-RUIZ
+ *
+ * Este script contiene la clase "Calculadora" que realiza operaciones de multiplicación y división.
+ * Fue creado por Rubén Ruiz Mejias.
+ * @copyrigth 2023
+ * @see Calculadora
+ *
+ */
+
+/**
+ * Clase Calculadora
+ *
+ * Esta clase representa una calculadora que puede realizar operaciones de multiplicación y división.
+ */
+class Calculadora
+{
+    /**
+     * Método multiplicacion
+     *
+     * Realiza la multiplicación de dos números.
+     *
+     * @param float $num1 El primer número.
+     * @param float $num2 El segundo número.
+     * @return float El resultado de la multiplicación.
+     * @version 1.0
+     */
+    public function multiplicacion($num1, $num2)
+    {
+        return $num1 * $num2;
+    }
+
+    /**
+     * Método division
+     *
+     * Realiza la división de dos números.
+     *
+     * @param float $num1 El numerador.
+     * @param float $num2 El denominador.
+     * @return float El resultado de la división.
+     * @throws Exception Si el denominador es cero.
+     * @version 1.0
+     */
+    public function division($num1, $num2)
+    {
+        if ($num2 == 0) {
+            throw new Exception("Error: División por cero.");
+        }
+        return $num1 / $num2;
+    }
+}
+
+// Ejemplo de uso de la clase Calculadora
+$calculadora = new Calculadora();
+echo "Actividad 2 - Script2-RUIZ.php - Clase Calculadora";
+
+$resultadoMultiplicacion = $calculadora->multiplicacion(5, 2);
+echo "<br/> Resultado de la multiplicación: " . $resultadoMultiplicacion;
+
+$resultadoDivision = $calculadora->division(10, 2);
+echo "<br/> Resultado de la división: " . $resultadoDivision;
